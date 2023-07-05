4ee7fb5a69aadd78df41279ae4b2dfa3ff36aea2
Fadekemystery <fadekemititilayo@gmail.com>
2023-07-05 18:26:44 +0100

    added firstjavaproject file

---
+++ FirstJava/src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome! This is my project");
+            System.out.println("Hello Hope, it's a new horizon");
+            System.out.printf("My Name is Fadekemi");
+        }
+    }
